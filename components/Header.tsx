341196ef1111f93abea165d37ad7afb7c60c111a
srikar kudurmalla <srikar2985969c@gmail.com>
2024-03-31 16:02:50 +0530

    feat: buttons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,27 +1,40 @@
 import Link from 'next/link'
 import React from 'react'
 import { Button } from "./ui/button";
-import {  Search } from "lucide-react";
+import { Search, CirclePlus, BellPlus, PlusCircle, User2 } from "lucide-react";
 import { calSans } from "@/app/fonts";
 type Props = {}
 
 export default function Header({}: Props) {
   return (
-      <header className="fixed bg-white top-0 flex items-center justify-between w-full z-50 border-b-2 border-neutral-800 px-9 py-2  backdrop-filter backdrop-blur-sm bg-opacity-70  ">
-          <Link href={"/dashboard"}>
+        <header className="fixed bg-white top-0 flex items-center justify-between w-full z-50 border-b-2 border-neutral-800 px-9 py-2  backdrop-filter backdrop-blur-sm bg-opacity-70  ">
+             <Link href={"/dashboard"}>
               <p className={`font-semibold text-xl ${calSans.className}`}>
                   Global Placements
               </p>
           </Link> 
           <div className="flex items-center space-x-2">
+
               <div className="flex items-center text-neutral-600 bg-zinc-100 gap-x-2 rounded-md px-3.5 py-1.5">
                   <Search className="h-4 w-4" />
                   
               </div>
+            
+                  <div className="flex items-center text-neutral-600 bg-zinc-100 gap-x-2 rounded-md px-3.5 py-1.5">
+                      <BellPlus className="h-4 w-4" />
+              </div>
+              <div className="flex items-center text-neutral-600 bg-zinc-100 gap-x-2 rounded-md px-3.5 py-1.5">
+                  <PlusCircle className="h-4 w-4" />
+              </div>
 
-              <Button size={"icon"}>
-                 admin
+              
+
+              <Button size={'sm'} variant={'secondary'}>
+                 Admin
               </Button>
+              <div className="flex items-center text-neutral-600 bg-zinc-100 gap-x-2 rounded-md px-3.5 py-1.5">
+                  <User2 className="h-4 w-4" />
+              </div>
           </div>
       </header> 
   )
