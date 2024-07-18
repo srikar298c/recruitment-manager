988f5ceed86c569fefa4ca5b18fece54c5cf5c92
srikar kudurmalla <srikar2985969c@gmail.com>
2024-07-18 22:37:36 +0530

    initail commit

---
+++ global-placements/rejections/scripts.js
@@ -0,0 +1,12 @@
+const currentFolder = window.location.pathname.split('/').slice(-2, -1)[0];
+
+// Get all sidebar links
+const sidebarLinks = document.querySelectorAll('.sidebar-item a');
+
+// Loop through the links and add the active class to the matching link
+sidebarLinks.forEach(link => {
+  const linkFolder = link.getAttribute('href').split('/').slice(-2, -1)[0];
+  if (linkFolder === currentFolder) {
+    link.parentElement.classList.add('active');
+  }
+});
